fd1c6cff323b12e2e6fcc5a62bd46eff37373ee0
Jan-Joven Jumao-as <janjoven.jumaoas@gmail.com>
2020-09-24 12:20:50 +0800

    maja url fix

--- config/providerlinks.php
+++ config/providerlinks.php
@@ -260,7 +260,7 @@ return [
         'auth' => 'wsLQrQM1OC1bVscK',
         'provider_id' => 39,
         'prefix' => 'MAJA_', 
-        'api_url'=> 'https://api.sagq3ktbaxo.com/api/MOGI/', //slot api
+        'api_url'=> 'https://api.sagq3ktbaxo.com/api/MOGI', //slot api
         'tapbet_api_url'=> 'https://tbb.sagq3ktbaxo.com/api', //tapbet api
     ],
     'spade_curacao'=>[
