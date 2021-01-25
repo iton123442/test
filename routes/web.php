401badf0e06cb31d79be2cc683b0bd5fb6eea3ae
unknown <riandraft@gmail.com>
2021-01-25 16:42:12 +0800

    Route Web Tigergames

--- routes/web.php
+++ routes/web.php
@@ -524,3 +524,7 @@ $app->post('/api/oryx/readWriteProcess', 'OryxGamingController@readWriteProcess'
 $app->post('/api/oryx/fundTransfer', 'OryxGamingController@fundTransfer');
 $app->post('tigergames/{type}/bg-fundtransfer','FundtransferProcessorController@backgroundProcessDebitCreditFund');
 $app->post('tigergames/bg-fundtransfer','FundtransferProcessorController@bgFundTransfer');
+
+
+$app->post('tigergames/fundtransfer','FundtransferProcessorController@fundTransfer');
+$app->post('tigergames/fundtransfer-timeout','FundtransferProcessorController@fundTransferTimout');
