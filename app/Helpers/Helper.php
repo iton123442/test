6407ccea11129a78c4d0aa96e0be99dc7ae4cd50
janjoven <janjoven.jumaoas@gmail.com>
2021-02-27 11:33:20 +0800

    change to progressing

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -241,7 +241,7 @@ class Helper
 			case "debit":
 					$trans_data["provider_trans_id"] = $request_data["transid"];
 					$trans_data["bet_amount"] = abs($request_data["amount"]);
-					$trans_data["win"] = 0;
+					$trans_data["win"] = 5;
 					$trans_data["pay_amount"] = 0;
 					$trans_data["entry_id"] = 1;
 					$trans_data["income"] = 0;
