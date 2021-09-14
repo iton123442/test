9ef83dfbc24b5027a3c29f677283d31c752920ff
linker.gitlab <marvinvillanea1@gmail.com>
2021-09-14 22:31:10 +0800

    UPDATE

--- app/Http/Controllers/GameLobby/GameLobbyController.php
+++ app/Http/Controllers/GameLobby/GameLobbyController.php
@@ -268,7 +268,7 @@ class GameLobbyController extends Controller
                     return response($msg,200)
                     ->header('Content-Type', 'application/json');
                 }
-                elseif($provider_code==74){
+                elseif($provider_code== 74 || $provider_code == 115  || $provider_code == 119 ){
                     $msg = array(
                         "game_code" => $request->input("game_code"),
                         "url" => GameLobby::evolutionLaunchUrl($request->game_code,$token,$request->input('game_provider'),$request->exitUrl,$ip_address,$lang),
