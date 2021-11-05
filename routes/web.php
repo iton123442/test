c54409ec9f428a3e7b4bf6b2a691c483f3753b13
bluespicyhead <janjoven.jumaoas@gmail.com>
2021-11-05 16:26:23 +0800

    merge the png betsofted

--- routes/web.php
+++ routes/web.php
@@ -293,6 +293,12 @@ $app->post('/public/api/png/reserve','PNGController@reserve');
 $app->post('/public/api/png/release','PNGController@release');
 $app->post('/public/api/png/balance','PNGController@balance');
 $app->post('/public/api/png/cancelReserve','PNGController@cancelReserve');
+//PNG BETSOFTED
+$app->post('/api/png/betsofted/authenticate','PNGControllerBetsofted@authenticate');
+$app->post('/api/png/betsofted/reserve','PNGControllerBetsofted@reserve');
+$app->post('/api/png/betsofted/release','PNGControllerBetsofted@release');
+$app->post('/api/png/betsofted/balance','PNGControllerBetsofted@balance');
+$app->post('/api/png/betsofted/cancelReserve','PNGController@cancelReserve');
 //Wazdan Endpoints
 $app->post('/public/api/wazdan/authenticate','WazdanController@authenticate');
 $app->post('/public/api/wazdan/getStake','WazdanController@getStake');
