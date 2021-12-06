736a68e227d63258735b5c7f5c4073b37c2bbaac
zhack01 <zhack001@gmail.com>
2021-12-06 17:08:56 +0800

    update amuse gaming test

--- app/Helpers/AmuseGamingHelper.php
+++ app/Helpers/AmuseGamingHelper.php
@@ -157,7 +157,7 @@ class AmuseGamingHelper{
              ******************************************************************/
             $before = microtime();
             $before_tz = date_default_timezone_get();
-            date_default_timezone_set("America/Chicago");
+            date_default_timezone_set("UTC");
             $after = microtime();
             $after_tz = date_default_timezone_get();
             return $before." , ".$after." , ".$before_tz." , ". $after_tz;
