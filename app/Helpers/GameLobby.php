d29dc5f8dc24320340383472cb285b0a946545b7
linker.gitlab <marvinvillanea1@gmail.com>
2021-12-14 11:10:35 +0800

    UPDATE

--- app/Helpers/GameLobby.php
+++ app/Helpers/GameLobby.php
@@ -575,7 +575,6 @@ class GameLobby{
         try{
             $url = config('providerlinks.tidygaming.url_lunch');
             $client_details = Providerhelper::getClientDetails('token', $token);
-
             // $invite_code = config('providerlinks.tidygaming.usd_invite');
             // if ($client_details->default_currency == "THB" ) {
             //     $invite_code = config('providerlinks.tidygaming.thb_invite');
@@ -585,7 +584,6 @@ class GameLobby{
             // $invite_code = config('providerlinks.tidygaming.currency')[$client_details->default_currency];
             
             // $get_code_currency = TidyHelper::currencyCode($client_details->default_currency);
-
             $operator_currency_support_1_1_using_prefix_k = [168,245,247,248,249,250];//client_id
             $currency = $client_details->default_currency;
             if (in_array( $client_details->client_id, $operator_currency_support_1_1_using_prefix_k)) {
