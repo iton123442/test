c251669bd5a13e46818d6b0869e6d7efa28e3492
zhack01 <zhack001@gmail.com>
2022-01-05 18:28:46 +0800

    udpate device

--- app/Helpers/GameLobby.php
+++ app/Helpers/GameLobby.php
@@ -919,7 +919,7 @@ class GameLobby{
                 'err_line' => $e->getLine(),
                 'err_file' => $e->getFile()
             );
-            ProviderHelper::saveLogGameLaunch('pragmatic gamelaunch err', 26, json_encode($msg), $e->getMessage());
+            ProviderHelper::saveLog('pragmatic gamelaunch err', 26, json_encode($msg), $e->getMessage());
             return $msg;
         }
         // $paramEncoded = urlencode("token=".$token."&symbol=".$game_code."&technology=H5&platform=WEB&language=en&lobbyUrl=daddy.betrnk.games");
