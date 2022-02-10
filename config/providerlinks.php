2231efd98d059783f58a207919ff33cce8bb45e9
linker.gitlab <marvinvillanea1@gmail.com>
2022-02-10 10:55:50 +0800

    UPDATE

--- config/providerlinks.php
+++ config/providerlinks.php
@@ -216,8 +216,12 @@ return [
             'kLAK' => '951a128d',
             'kMMK' => '1d69f90a',
             'kKHR' => '3d7edfac',
+            'VND'  => 'b807860d',
+            'LAK'  => '223f24ab',
+            'MMK'  => '89853cff',
+            'KHR'  => 'd264b6ad',
         ],
-        'support_1to1_denomination_prefixK' => [245,247,248,249,250,185], //THIS IS THE CLIENT ID THAT SUPPOR PREFIX K OR 1:1 EXAMPLE currency LAK concant for kLAK
+        'support_1to1_denomination_prefixK' => [], //THIS IS THE CLIENT ID THAT SUPPOR PREFIX K OR 1:1 EXAMPLE currency LAK concant for kLAK
         'SECRET_KEY' => 'f83c8224b07f96f41ca23b3522c56ef1',
         'TransferWallet' => [
             "client_id" => '2efa763b',
