6e196a496fb93544d4d88f51a91e78eff7daf567
Al Highnurr <jael777ster@gmail.com>
2022-02-25 14:55:07 +0800

    spearhead update fs

--- app/Helpers/FreeSpinHelper.php
+++ app/Helpers/FreeSpinHelper.php
@@ -794,7 +794,7 @@ class FreeSpinHelper{
             return 400;
         } else {
             $data = [
-                "details" => json_encode($dataresponse)
+                "details" => json_encode($dataresponse->items["bonus_id"])
             ];
             FreeSpinHelper::updateFreeRound($data, $id);
             $freespinExtenstion = [
