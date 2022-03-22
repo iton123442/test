f78303622b704016406b9d1111c13df4fd2c7697
linker.gitlab <marvinvillanea1@gmail.com>
2022-03-22 16:30:35 +0800

    UPDATE

--- app/Http/Controllers/TidyController.php
+++ app/Http/Controllers/TidyController.php
@@ -540,6 +540,18 @@ class TidyController extends Controller
 			Helper::saveLog('Tidy Rollback error', $this->provider_db_id, json_encode(file_get_contents("php://input")), $data_response);
 			return $data_response;
 		}
+		$bet_transaction = GameTransactionMDB::findGameExt($reference_transaction_uuid, 3,'transaction_id', $client_details);
+		if ($bet_transaction != 'false') {
+			$response = [
+				"uid" => $uid,
+				"request_uuid" => $request_uuid,
+				"currency" => TidyHelper::getcurrencyCode($client_details->default_currency,$client_details->client_id),
+				"balance" =>  ProviderHelper::amountToFloat($client_details->balance)
+			];
+			Helper::saveLog('Tidy BET duplicate_transaction success', $this->provider_db_id, json_encode($request->all()),  $bet_transaction->mw_response);
+			return response($response,200)
+			->header('Content-Type', 'application/json');
+		} 
 		try{
 	 		ProviderHelper::idenpotencyTable($this->prefix.'_'.$transaction_uuid);
 		}catch(\Exception $e){
