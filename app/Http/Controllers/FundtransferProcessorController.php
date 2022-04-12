b95e32a84021469d65500b35e366ec1e23c6dcfe
Elvis <elvis.pionita@gmail.com>
2022-04-12 19:12:43 +0800

    ps ulahi update

--- app/Http/Controllers/FundtransferProcessorController.php
+++ app/Http/Controllers/FundtransferProcessorController.php
@@ -49,7 +49,7 @@ class FundtransferProcessorController extends Controller
             }else if ($payload->action->custom->provider == "OnlyPlay") {
                 $gteid = $payload->action->custom->game_trans_ext_id;
             }else if ($payload->action->custom->provider == "PlayStar Gaming") {
-                $gteid = $payload->action->custom->game_transaction_ext_id;
+                $gteid = $payload->action->custom->game_trans_ext_id;
             }else if ($payload->action->custom->provider == "Nolimit City") {
                 $gteid = $payload->action->custom->game_trans_ext_id;
             }else if ($payload->action->custom->provider == "BGaming") {
