bb26b3dcad47d32aad5fffba4e1f29bbd7411200
linker.gitlab <marvinvillanea1@gmail.com>
2022-04-14 03:50:53 +0800

    update job Creategamelog

--- app/Jobs/CreateGameTransactionLog.php
+++ app/Jobs/CreateGameTransactionLog.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Models\GameTransactionMDB;
+use App\Helpers\ClientRequestHelper;
 
 class CreateGameTransactionLog extends Job
 {
@@ -44,6 +45,13 @@ class CreateGameTransactionLog extends Job
             $game_trans_ext_id = $payload["game_trans_ext_id"];
             $updateDAta = $payload['column'];
             GameTransactionMDB::updateGametransactionLog($updateDAta,$game_trans_ext_id,false,$client_details);
+        }
+
+        if($payload["type"] == "cutcall_update") {
+            $connection = $payload['connection'];
+            $game_trans_ext_id = $payload["game_trans_ext_id"];
+            $updateDAta = $payload['column'];
+            ClientRequestHelper::updateGametransactionLogEXTCCMD($updateDAta, $game_trans_ext_id, $connection);
         }
         
     }
