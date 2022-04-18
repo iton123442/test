d6d1aec037c0bb4001a0eb9bcddc4e99a33bfde7
unknown <zhack001@gmail.com>
2022-04-18 10:51:36 +0800

    update ygg player session

--- app/Http/Controllers/YGG002Controller.php
+++ app/Http/Controllers/YGG002Controller.php
@@ -71,8 +71,8 @@ class YGG002Controller extends Controller
         $game_details = Helper::findGameDetails('game_code', $this->provider_id, $request->cat5);
         
         # Check Game Restricted
-		
-        if($client_details == null || $game_details == null){ 
+		dd($game_details);
+        if($client_details == null){ 
             $response = array(
                 "code" => 1000,
                 "msg" => "Session expired. Please log in again."
