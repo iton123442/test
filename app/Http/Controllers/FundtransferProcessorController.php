1b0be267e94fb8cf984ba656411637e5c558bfba
Elvis <elvis.pionita@gmail.com>
2022-04-26 11:26:57 +0800

    new update ps

--- app/Http/Controllers/FundtransferProcessorController.php
+++ app/Http/Controllers/FundtransferProcessorController.php
@@ -426,11 +426,12 @@ class FundtransferProcessorController extends Controller
                                     "transaction_detail" =>json_encode("success"),
                                     "general_details" =>json_encode("success")
                                 );
-                                ClientRequestHelper::updateGametransactionEXTCCMD($ext_data, $gteid, $payload->action->custom->client_connection_name);
+                                //ClientRequestHelper::updateGametransactionEXTCCMD($ext_data, $gteid, $payload->action->custom->client_connection_name);
                                 $updateGameTransaction = [
                                     "win" => $payload->action->custom->win_or_lost,
                                 ];
                                 ClientRequestHelper::updateGameTransactionCCMD($updateGameTransaction, $payload->action->mwapi->roundId, $payload->action->custom->client_connection_name);
+                                ProviderHelper::queLogs($payload->action->custom->client_connection_name, $payload->action->custom->game_trans_ext_id, $requesttocient, $client_response, "client_details", "success");
                             }
                             elseif ($payload->action->custom->provider == 'PlayStar Gaming') {
                                 $ext_data = array(
