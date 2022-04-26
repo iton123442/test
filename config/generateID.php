39b86af7fa0dd925868cbdae29135ab8ea37402a
linker.gitlab <marvinvillanea1@gmail.com>
2022-04-27 04:36:40 +0800

    UPDATE

--- config/generateID.php
+++ config/generateID.php
@@ -4,28 +4,28 @@
 return [
     'generate_id'=>[
         "default" => [
-            'game_trans_id'=> 100000000000000000,
-            'game_transaction_ext' => 400000000000000000,
+            'game_trans_id'=> 1000000000000000000,
+            'game_transaction_ext' => 4000000000000000000,
         ],
         "server1-askmebet" => [
-            'game_trans_id'=> 200000000000000000,
-            'game_transaction_ext' => 200000000000000000,
+            'game_trans_id'=> 1000000000000000000,
+            'game_transaction_ext' => 4000000000000000000,
         ],
         "server2-askmebet" =>[
-            'game_trans_id'=> 100000000000000000,
-            'game_transaction_ext' => 100000000000000000,
+            'game_trans_id'=> 1000000000000000000,
+            'game_transaction_ext' => 4000000000000000000,
         ],
         "server3" =>[
-            'game_trans_id'=> 100000000000000000,
-            'game_transaction_ext' => 100000000000000000,
+            'game_trans_id'=> 1000000000000000000,
+            'game_transaction_ext' => 4000000000000000000,
         ],
         "mysql" => [
-            'game_trans_id'=> 100,
-            'game_transaction_ext' => 100,
+            'game_trans_id'=> 1000000000000000000,
+            'game_transaction_ext' => 4000000000000000000,
         ],
         "server_TW" => [
-            'game_trans_id'=> 100000000000000000,
-            'game_transaction_ext' => 100000000000000000,
+            'game_trans_id'=> 1000000000000000000,
+            'game_transaction_ext' => 4000000000000000000,
         ],
     ]
 ];
