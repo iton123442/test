617d28d0b837cfc8fe86f6dbe3fdc39bbd9148fd
Al Highnurr <jael777ster@gmail.com>
2022-04-27 08:39:34 +0800

    update ttg id gen

--- app/Http/Controllers/TTGController.php
+++ app/Http/Controllers/TTGController.php
@@ -237,7 +237,7 @@ class TTGController extends Controller
                                           "connection_name" => $get_client_details->connection_name,
                                           "column" =>[
                                               "game_trans_ext_id" => $gen_game_extid,
-                                              "request" => json_encode($request->all()),
+                                              "request" => json_encode($data),
                                               "response" => json_encode($response),
                                               "log_type" => "provider_details",
                                               "transaction_detail" => "FAILED",
@@ -255,7 +255,7 @@ class TTGController extends Controller
                                           "connection_name" => $get_client_details->connection_name,
                                           "column" =>[
                                               "game_trans_ext_id" => $gen_game_extid,
-                                              "request" => json_encode($request->all()),
+                                              "request" => json_encode($data),
                                               "response" => json_encode($response),
                                               "log_type" => "provider_details",
                                               "transaction_detail" => "FAILED",
@@ -301,7 +301,7 @@ class TTGController extends Controller
                                   "connection_name" => $get_client_details->connection_name,
                                   "column" =>[
                                       "game_trans_ext_id" => $gen_game_extid,
-                                      "request" => json_encode($request->all()),
+                                      "request" => json_encode($data),
                                       "response" => json_encode($response),
                                       "log_type" => "provider_details",
                                       "transaction_detail" => "SUCCESS",
