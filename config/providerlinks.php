cb4a08d998b86b8722789b78f0697dfff998d345
linker.gitlab <marvinvillanea1@gmail.com>
2022-05-11 04:37:22 +0800

    UPDATE

--- config/providerlinks.php
+++ config/providerlinks.php
@@ -221,6 +221,9 @@ return [
             'LAK'  => '223f24ab',
             'MMK'  => '89853cff',
             'KHR'  => 'd264b6ad',
+            'TWD'  => '7eaa51c7',
+            'INR'  => 'ccc1956b',
+            'GBP'  => 'e68d2044',
         ],
         'support_1to1_denomination_prefixK' => [188,142,185], //THIS IS THE CLIENT ID THAT SUPPOR PREFIX K OR 1:1 EXAMPLE currency LAK concant for kLAK
         'SECRET_KEY' => 'f83c8224b07f96f41ca23b3522c56ef1',
