2492501b42d6159db7b40641a6c2a9d169778dd9
linker.gitlab <marvinvillanea1@gmail.com>
2022-08-31 18:37:49 +0800

    update

--- app/Http/Controllers/LoadTesterQueryCLientCallController.php
+++ app/Http/Controllers/LoadTesterQueryCLientCallController.php
@@ -16,9 +16,32 @@ class LoadTesterQueryCLientCallController extends Controller
     }
     public function ProcessTransaction(Request $request){
         $data = json_decode($request->getContent(),TRUE);
-        Helper::saveLog('BNGMETHOD(BNG)', 12, json_encode(["request_data" => $data]), "");
+        // Helper::saveLog('BNGMETHOD(BNG)', 12, json_encode(["request_data" => $data]), "");
         $client_details = ProviderHelper::getClientDetails('token', $data["token"]);
         $game_details = ProviderHelper::findGameDetails('game_code', $this->prefix, $data["game_id"]);
+
+        $win_or_lost = $data["args"]["win"] == 0 ? 0 : 1;
+        $gameTransactionData = array(
+            "provider_trans_id" => $data["uid"],
+            "token_id" => $client_details->token_id,
+            "game_id" => $game_details->game_id,
+            "round_id" => $data["args"]["round_id"],
+            "bet_amount" => $data["args"]["bet"],
+            "win" => 5,
+            "pay_amount" =>$data["args"]["win"],
+            "income" =>$data["args"]["bet"]-$data["args"]["win"],
+            "entry_id" =>$data["args"]["win"] == 0 ? 1 : 2,
+        );
+        $game_transactionid = GameTransactionMDB::createGametransaction($gameTransactionData,$client_details);
+        $betgametransactionext = array(
+            "game_trans_id" => $game_transactionid,
+            "provider_trans_id" => $data["uid"],
+            "round_id" => $data["args"]["round_id"],
+            "amount" => $data["args"]["bet"],
+            "game_transaction_type"=>1,
+            "provider_request" =>json_encode($data),
+        );
+        $betGametransactionExtId = GameTransactionMDB::createGameTransactionExt($betgametransactionext,$client_details);
         $response =array(
             "uid"=>$data["uid"],
             "balance" => array(
