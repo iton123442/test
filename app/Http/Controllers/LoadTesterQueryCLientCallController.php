07f94015e55654d9344259e40ef552e2897a2db8
linker.gitlab <marvinvillanea1@gmail.com>
2022-09-04 21:10:19 +0800

    update load tester

--- app/Http/Controllers/LoadTesterQueryCLientCallController.php
+++ app/Http/Controllers/LoadTesterQueryCLientCallController.php
@@ -20,39 +20,39 @@ class LoadTesterQueryCLientCallController extends Controller
         $client_details = ProviderHelper::getClientDetails('token', $data["token"]);
         $game_details = ProviderHelper::findGameDetails('game_code', $this->prefix, $data["game_id"]);
 
-        $win_or_lost = $data["args"]["win"] == 0 ? 0 : 1;
-        $gameTransactionData = array(
-            "provider_trans_id" => $data["uid"],
-            "token_id" => $client_details->token_id,
-            "game_id" => $game_details->game_id,
-            "round_id" => $data["args"]["round_id"],
-            "bet_amount" => $data["args"]["bet"],
-            "win" => 5,
-            "pay_amount" =>$data["args"]["win"],
-            "income" =>$data["args"]["bet"]-$data["args"]["win"],
-            "entry_id" =>$data["args"]["win"] == 0 ? 1 : 2,
-        );
-        $game_transactionid = GameTransactionMDB::createGametransaction($gameTransactionData,$client_details);
-        $betgametransactionext = array(
-            "game_trans_id" => $game_transactionid,
-            "provider_trans_id" => $data["uid"],
-            "round_id" => $data["args"]["round_id"],
-            "amount" => $data["args"]["bet"],
-            "game_transaction_type"=>1,
-            "provider_request" =>json_encode($data),
-        );
-        $betGametransactionExtId = GameTransactionMDB::createGameTransactionExt($betgametransactionext,$client_details);
-        $balance = number_format($client_details->balance,2,'.', '');
-        $client_details->balance = $balance;
-        ProviderHelper::_insertOrUpdate($client_details->token_id, $balance);
+        // $win_or_lost = $data["args"]["win"] == 0 ? 0 : 1;
+        // $gameTransactionData = array(
+        //     "provider_trans_id" => $data["uid"],
+        //     "token_id" => $client_details->token_id,
+        //     "game_id" => $game_details->game_id,
+        //     "round_id" => $data["args"]["round_id"],
+        //     "bet_amount" => $data["args"]["bet"],
+        //     "win" => 5,
+        //     "pay_amount" =>$data["args"]["win"],
+        //     "income" =>$data["args"]["bet"]-$data["args"]["win"],
+        //     "entry_id" =>$data["args"]["win"] == 0 ? 1 : 2,
+        // );
+        // $game_transactionid = GameTransactionMDB::createGametransaction($gameTransactionData,$client_details);
+        // $betgametransactionext = array(
+        //     "game_trans_id" => $game_transactionid,
+        //     "provider_trans_id" => $data["uid"],
+        //     "round_id" => $data["args"]["round_id"],
+        //     "amount" => $data["args"]["bet"],
+        //     "game_transaction_type"=>1,
+        //     "provider_request" =>json_encode($data),
+        // );
+        // $betGametransactionExtId = GameTransactionMDB::createGameTransactionExt($betgametransactionext,$client_details);
+        // $balance = number_format($client_details->balance,2,'.', '');
+        // $client_details->balance = $balance;
+        // ProviderHelper::_insertOrUpdate($client_details->token_id, $balance);
 
-        $response =array(
-            "uid"=>$data["uid"],
-            "balance" => array(
-                "value" => (string)$client_details->balance,
-                "version" => round(microtime(true) * 1000)//$this->_getExtParameter()
-            ),
-        );
+        // $response =array(
+        //     "uid"=>$data["uid"],
+        //     "balance" => array(
+        //         "value" => (string)$client_details->balance,
+        //         "version" => round(microtime(true) * 1000)//$this->_getExtParameter()
+        //     ),
+        // );
         return response($response,200)
             ->header('Content-Type', 'application/json');
     }
