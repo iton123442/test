2ff432c80a3bb9b9a06d91cd87d3760242c41dda
linker.gitlab <marvinvillanea1@gmail.com>
2022-10-06 03:09:51 +0800

    update

--- config/providerlinks.php
+++ config/providerlinks.php
@@ -324,10 +324,7 @@ return [
     'bgaming' => [
         'PROVIDER_ID'=> 49,
         'CASINO_ID' => 'tigergames-int',
-        'brand' => [
-            "11" => "KONIBET",
-            "1" => "tigergames-int",
-        ],
+        "KONIBET" => "KONIBET",
         'GCP_URL' => 'https://int.bgaming-system.com/a8r/tigergames-int',
         'AUTH_TOKEN' => 'HZhPwLMXtHrmQUxjmMvBmCPM'
    ],
