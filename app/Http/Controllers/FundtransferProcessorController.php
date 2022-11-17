10203e1fdb723db6d31f87b6797b9b04fec513ef
Rynz <rynz.hipolan@gmail.com>
2022-11-17 14:38:18 +0800

    Update: AWS added

--- app/Http/Controllers/FundtransferProcessorController.php
+++ app/Http/Controllers/FundtransferProcessorController.php
@@ -179,7 +179,7 @@ class FundtransferProcessorController extends Controller
                                 ClientRequestHelper::updateGameTransactionCCMD($updateGameTransaction, $payload->action->mwapi->roundId, $payload->action->custom->client_connection_name);
 
                                 // V2 EXT UPDATE
-                                ProviderHelper::queLogs($payload->action->custom->client_connection_name, $payload->action->custom->game_transaction_ext_id, $requesttocient, $client_response, "client_details", "success");
+                                // ProviderHelper::queLogs($payload->action->custom->client_connection_name, $payload->action->custom->game_transaction_ext_id, $requesttocient, $client_response, "client_details", "success");
 
                             }
                             elseif($payload->action->custom->provider == 'kagaming'){
