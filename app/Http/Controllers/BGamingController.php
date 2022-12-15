81d01ad96abe480b8f669cd0635b78662815af5a
RiAN <jael777ster@gmail.com>
2022-12-15 14:41:13 +0800

    update (reject rollback if win is already received)

--- app/Http/Controllers/BGamingController.php
+++ app/Http/Controllers/BGamingController.php
@@ -706,6 +706,36 @@ class BGamingController extends Controller
             Helper::saveLog('Bgaming WIN RESPONSE', $this->provider_db_id, json_encode($data), $response);
             return $response;
         }
+        $getGameExtWin = GameTransactionMDB::findGameExt($round_id, 2,'round_id', $client_details);
+        if($getGameExtWin == 'false'){
+            $balance = number_format(round($client_details->balance,2),2,'.','');
+            $balance = str_replace(".", "", $balance);
+            $response = [
+                "balance" => (float)$balance,
+                "game_id" => $data['game_id'],
+                "transactions" =>[
+                    [
+                        "action_id" =>$data['actions'][0]['action_id'],
+                        "tx_id" =>  $getGameExtWin->game_trans_ext_id,
+                        "processed_at" => $processtime->format('Y-m-d\TH:i:s.u'),
+                    ],
+                ],
+            ];
+            $game_transaction_extension = array(
+                "game_trans_id" => $bet_transaction->game_trans_id,
+                "provider_trans_id" => $transactionId,
+                "round_id" => $round_id,
+                "amount" => $amount,
+                "game_transaction_type"=> 3,
+                "provider_request" => json_encode($data),
+                "mw_response" => json_encode($response),
+                "mw_request" => "This transaction has win!!",
+                "mw_response" => "This transaction has win!!",
+                "transaction_detail" => "FAILED"
+            );
+            $winGametransactionExtId = GameTransactionMDB::createGameTransactionExt($game_transaction_extension,$client_details);
+            return $response;
+        }
         $isGameExtFailed = GameTransactionMDB::findGameExt($original_ID_transaction, 1,'transaction_id', $client_details);
         $amount = $isGameExtFailed->amount;
         if($isGameExtFailed != 'false'){ 
