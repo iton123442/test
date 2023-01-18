b38c7f85688a311ea6fba47864683f2de1d14cb5
RiAN <jael777ster@gmail.com>
2023-01-18 14:46:28 +0800

    update wazdan get round history

--- app/Http/Controllers/WazdanNewV2Controller.php
+++ app/Http/Controllers/WazdanNewV2Controller.php
@@ -759,7 +759,7 @@ class WazdanNewV2Controller extends Controller
     public function getTransactionHistory(Request $request){
         $data = $request->getContent();
         $datadecoded = json_decode($data,TRUE);
-        // dd($request->all());
+        dd($request->all());
         Helper::saveLog('RoundHistory(Wazdan)', 33, $data, "Initialize");
         $key = "uTDVNr4wu6Y78SNbr36bqsSCH904Rcn1";
         $operator_data = config('providerlinks.wazdan.operator_data');
