5b9528b7812142ba2b0eb35a72dcbada7eaf726b
RiAN <jael777ster@gmail.com>
2023-01-19 11:53:18 +0800

    update wazdan get transdetails

--- app/Http/Controllers/WazdanNewV2Controller.php
+++ app/Http/Controllers/WazdanNewV2Controller.php
@@ -808,7 +808,7 @@ class WazdanNewV2Controller extends Controller
         } catch ( \Exception $e) {
             $res = json_encode($e);
         }
-        return $res
+        return $res;
         
     }
    
