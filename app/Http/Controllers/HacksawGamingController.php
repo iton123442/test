ead0acafebf7bc4d0e89bfd797b28e27365d7af0
Elvis <elvis.pionita@gmail.com>
2023-01-26 08:48:07 +0800

    new update hg

--- app/Http/Controllers/HacksawGamingController.php
+++ app/Http/Controllers/HacksawGamingController.php
@@ -987,7 +987,6 @@ class HacksawGamingController extends Controller
     public function Gamelist(Request $request){
         $partner_id = "tigergames_stg";
         $endpoint = "https://api.hacksawgaming.com/api/v1/meta/".$partner_id."/gameList";
-
         $client = new GuzzleHttp\Client();
         $response = $client->request('GET', $endpoint, [
             'query' => [
