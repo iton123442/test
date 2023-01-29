7c4584fab402f9ba8abc0d256feaff09d6fb8dc8
RiAN <jael777ster@gmail.com>
2023-01-29 21:12:44 +0800

    update20230129

---
+++ app/Http/Controllers/QTechController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Helpers\Helper;
+use App\Helpers\ProviderHelper;
+use GuzzleHttp\Client;
+use App\Helpers\ClientRequestHelper;
+use App\Models\GameTransaction;
+use App\Models\GameTransactionMDB;
+use App\Helpers\FreeSpinHelper;
+use App\Helpers\Game;
+use Carbon\Carbon;
+use DB;
+
+class QtechController extends Controller
+{
+    public function verifySession(Request $request){
+        dd($request->all());
+    }
+}
