01f4284190b56e6ea917d66855a4bbea4d0c8959
RiAN <jael777ster@gmail.com>
2023-01-30 22:19:20 +0800

    update.Qt

--- app/Helpers/ProviderHelper.php
+++ app/Helpers/ProviderHelper.php
@@ -1359,7 +1359,7 @@ class ProviderHelper{
 		$client = new Client();
 		$response = $client->get($url);
         $data = json_decode($response->getBody(), true);
-        dd($data);
+        Helper::saveLog('accesstokenQT', 144, $data, "HIT");
         return $data['access_token'];
 	}
 	public static function simplePlayAPICall ($queryString, $hashedString) {     
